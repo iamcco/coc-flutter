959a6ee1a6c9ee337624839f78b4cacc9cac85ad
iamcco <ooiss@qq.com>
2019-10-08 17:30:35 +0800

    feat(hotreload): do not reload if there are errors for the save file

--- src/provider/hotreload.ts
+++ src/provider/hotreload.ts
@@ -1,5 +1,5 @@
-import {workspace, ConfigurationChangeEvent, Disposable} from 'coc.nvim'
-import {TextDocument} from 'vscode-languageserver-protocol';
+import {workspace, ConfigurationChangeEvent, Disposable, diagnosticManager} from 'coc.nvim'
+import {TextDocument, DiagnosticSeverity} from 'vscode-languageserver-protocol'
 
 import {devServer} from '../server/dev';
 
@@ -9,7 +9,13 @@ export const registerHotReloadProvider = (): Disposable => {
   if (enableHotReload) {
     subscription = workspace.onDidSaveTextDocument((doc: TextDocument) => {
       if (doc.languageId === 'dart' && devServer.state) {
-        devServer.sendCommand('r')
+        // do not reload if there ara errors for the save file
+        const diagnostics = diagnosticManager.getDiagnostics(doc.uri)
+        const hasErrors = diagnostics
+          && diagnostics.find((d) => d.source === "dart" && d.severity === DiagnosticSeverity.Error) != null
+        if (!hasErrors) {
+          devServer.sendCommand('r')
+        }
       }
     })
   }
