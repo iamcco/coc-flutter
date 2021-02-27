32453f9feac5da8c90b78a9ebd0bfe3a80dddfa1
Pieter van Loon <git@pietervanloon.com>
2021-02-27 14:10:02 +0100

    feat(restart-lsp): Added manual lsp restart command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,10 +3,11 @@ import { Dev } from './dev';
 import { Global } from './global';
 import { LspServer } from '../server/lsp';
 import { SuperCommand } from './super';
+import { LspCommands } from './lsp';
 
 export class Commands extends Dispose {
   constructor(lsp: LspServer) {
     super();
-    this.push(new Dev(), new Global(), new SuperCommand(lsp));
+    this.push(new Dev(), new Global(), new LspCommands(lsp), new SuperCommand(lsp));
   }
 }
