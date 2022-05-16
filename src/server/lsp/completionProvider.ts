3f3fb47c0387fcf24bcc2561707dc69cffe194b9
PENG Rui <1203802652@qq.com>
2022-05-16 11:30:10 +0800

    fix(LSP): fix isInComplete TypeError (close #153) (#154)

--- src/server/lsp/completionProvider.ts
+++ src/server/lsp/completionProvider.ts
@@ -20,10 +20,10 @@ export const completionProvider = async (
   const res = await next(document, position, context, token);
   let list: CompletionItem[];
   // CompletionItem[] or CompletionList
-  if ((res as CompletionList).isIncomplete !== undefined) {
+  if ((res as CompletionList)?.isIncomplete !== undefined) {
     list = (res as CompletionList).items;
   } else {
-    list = res as CompletionItem[];
+    list = res as CompletionItem[] ?? [];
   }
   const config = workspace.getConfiguration('dart');
   const resolveCompleteItem = getResolveCompleteItemFunc({
@@ -31,7 +31,7 @@ export const completionProvider = async (
   });
   // resolve complete item
   list = list.map(resolveCompleteItem);
-  return (res as CompletionList).isIncomplete !== undefined
+  return (res as CompletionList)?.isIncomplete !== undefined
     ? {
         items: list,
         isIncomplete: (res as CompletionList).isIncomplete,
